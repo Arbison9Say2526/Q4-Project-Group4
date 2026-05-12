8f770a00b28500d9d0a9b15c51220a3f19a474f1
Gabia9S-ay2526 <lea.miller130@gmail.com>
2026-05-12 08:01:00 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+let attempt_counter = 0;
+
+
+function LogUser() {
+    
+    // 1. Get references to the elements
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let display = document.getElementById("display");
+    let login = document.getElementById("login");
+
+    if (username === "KPasco" && password === "9Sapphire") {
+        
+        alert("Welcome, " + username + "!");
+        attempt_counter = 0;
+        window.location.href = 'dashboard.html';   // Change this if your homepage name is different
+        
+    } else {
+        attempt_counter++;
+        
+        if (attempt_counter >= 3) {
+            alert("Access denied. Account locked after 3 attempts!");
+            login.disabled = true;
+            display.innerHTML = "ACCOUNT LOCKED";
+        } else {
+            display.innerHTML = "Wrong username or password! Attempt " + attempt_counter + " of 3";
+        }
+    }
+}
