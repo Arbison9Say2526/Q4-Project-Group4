5d17dd38aa359f48a657803ae6a8190413ccdcdc
Gabia9S-ay2526 <lea.miller130@gmail.com>
2026-05-12 08:17:18 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,29 @@
 let attempt_counter = 0;
 
 
-function LogUser() {
-   // 1. Get references to the elements
-   let username = document.getElementById("username").value;
-   let password = document.getElementById("password").value;
-   let display = document.getElementById("display"); // Target the error message area
-   let loginBtn = document.getElementById("loginBTN");
+function LogUser() { 
+    event.preventDefault(); // This stops the page from refreshing!
 
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let display = document.getElementById("display");
+    let login = document.getElementById("login");
 
-   // 2. Declare your strings
-   let granted = "Welcome, " + username + "!";
-   let denied = "Access denied. Account locked after 3 attempts!";
-
-
-   // 3. Logic Check
-   if (username === "KPasco" && password === "9Sapphire") {
-       alert(granted);
-       attempt_counter = 0;
-       window.location.href = "ahomepage.html";
-   } else {
-       attempt_counter++;
-      
-       // Check if we hit the limit
-       if (attempt_counter >= 3) {
-           alert(denied);
-           loginBtn.disabled = true;
-           display.innerHTML = "<b>ACCOUNT LOCKED</b>";
-       } else {
-           display.innerHTML = "Incorrect credentials. Attempt " + attempt_counter + " of 3";
-       }
-   }
+    if (username === "KPasco" && password === "9Sapphire") {
+        
+        alert("Welcome, " + username + "!");
+        attempt_counter = 0;
+        window.location.href = 'ahomepage.html';   
+        
+    } else {
+        attempt_counter++;
+        
+        if (attempt_counter >= 3) {
+            alert("Access denied. Account locked after 3 attempts!");
+            login.disabled = true;
+            display.innerHTML = "ACCOUNT LOCKED";
+        } else {
+            display.innerHTML = "Wrong username or password! Attempt " + attempt_counter + " of 3";
+        }
+    }
 }
